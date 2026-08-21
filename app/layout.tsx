10f101f0006d0d2b48ac18c6cd89ea2aac2abe52
willyChairullah <willy.chairullah@gmail.com>
2026-08-21 14:41:17 +0700

    change my dymmy name

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Willy Pratama | Full-stack Web Developer",
-  description: "Portfolio of Willy Pratama, Full-stack Web Developer.",
+  title: "Willy | Web Developer",
+  description: "Portfolio of Willy Chairullah, Full-stack Web Developer.",
 };
 
 export default function RootLayout({
